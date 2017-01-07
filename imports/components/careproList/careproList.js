a00f46bde12f52ca8a37ba50e641fc82130d3a09
Wei Sheng <weisheng@leadbook.com>
2017-01-07 08:35:18 +0800

    Add matching carepro queries

--- imports/components/careproList/careproList.js
+++ imports/components/careproList/careproList.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { exampleCarePro, CarePro, generateRandomCarePro } from '../../api/carepro.js';
+import { VisitRequest } from '../../api/visitRequest.js';
 
 import template from './careproList.html'
 
@@ -10,6 +11,25 @@ class CareProListCtrl {
         this.helpers({
             carePros(){
                 return CarePro.find({})
+            },
+            matchedCarePros(){
+                var experienceQuery = {};
+                var willingQuery = {};
+                for (let task of Object.keys(VisitRequest.care_services)){
+                    var levels = VisitRequest.care_services[task];
+                    if (levels.length != 0){
+                        experienceQuery['skills_qualifications.task_level_experience.' + task] = {'$all': levels};
+                        willingQuery['skills_qualifications.task_level_willingness.' + task] = {'$all': levels};
+                    }
+                }
+                var willingAndExperienceds = CarePro.find({'$and': [experienceQuery, willingQuery]});
+                if (willingAndExperienceds.count() > 0){
+                    this.matched = "Willing and Experienced";
+                    return willingAndExperienceds;
+                } else {
+                    this.matched = "Willing"
+                    return CarePro.find(willingQuery);
+                }
             }
         });
     }
