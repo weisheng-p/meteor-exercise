d39184b523369ae488a91e73b1f8c752d086bbf8
Wei Sheng <weisheng@leadbook.com>
2017-01-07 08:30:14 +0800

    Add visit request example

---
+++ imports/api/visitRequest.js
@@ -0,0 +1,58 @@
+export const VisitRequest = {
+    "_id" : "73fXtuXY6QeA3j5m9",
+    "user_id" : "mMJDeo7iN67ce7vWh",
+    "city" : "Singapore",
+    "status" : "Requested",
+    "visit_location_address" : {
+        "street_address" : "72 Jalan Greja",
+        "post_code" : 488935,
+        "city_town" : "Singapore",
+        "country" : "Singapore"
+    },
+    "visit_schedule" : {
+        "date_from" : Date.parse("2016-12-26T00:00:00.000+0000"),
+        "date_to" : Date.parse("2016-12-31T00:00:00.000+0000"),
+        "start_time" : "4:30 PM",
+        "end_time" : "5:30 PM",
+        "repeat_days" : [
+            "Monday",
+            "Tuesday",
+            "Wednesday"
+         ]
+    },
+    "care_services" : {
+        "medication" : [
+
+        ],
+        "staying_active" : [
+
+        ],
+        "meal_preparation" : [
+
+        ],
+        "grocery_shopping" : [
+
+        ],
+        "transport_assistance" : [
+
+        ],
+        "housekeeping" : [
+            "Bed making"
+        ],
+        "personal_care_hygiene" : [
+            "Bed Bath",
+            "Change Diapers",
+            "Manual Transfers"
+        ],
+        "companionship" : [
+
+        ],
+        "checkin_visits" : [
+
+        ],
+        "night_caregiving" : [
+
+        ]
+    },
+    "created_at" : Date.parse("2016-12-17T02:43:37.017+0000")
+}
